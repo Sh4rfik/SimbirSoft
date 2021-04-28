3036dfe674764d2e648589958b4de44ac78c3551
Sh4rfik <80812692+Sh4rfik@users.noreply.github.com>
2021-04-28 12:46:27 +0300

    logging

---
+++ src/main/java/Main/Log.java
@@ -0,0 +1,27 @@
+package Main;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Log {
+
+    private static Logger logger = Logger.getLogger("MyLog");
+    private static FileHandler fh;
+
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    public static void logger() throws IOException {
+        FileHandler fh;
+        fh = new FileHandler("C:\\Users\\Administrator\\log.config", true);
+        logger.addHandler(fh);
+        SimpleFormatter formatter = new SimpleFormatter();
+        fh.setFormatter(formatter);
+    }
+
+}
